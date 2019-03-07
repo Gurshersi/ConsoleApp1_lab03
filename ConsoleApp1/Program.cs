c684cccfc080339c4950726b4a845e157ac830ad
visualstudio <visualstudio@230-PC10>
2019-03-07 16:00:49 -0500

    lab 03

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace DelegatesExercises
 {
-    class Program
-    {
-        static void Main(string[] args)
+    public class DelegateExercises
+    { public delegate void MyDelegate();
+    void method(int i)
         {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
